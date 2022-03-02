b64b58227bbcacff13c7e5a5e1059cfff792c2cb
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2022-03-02 11:53:26 +0100

    Let's start

---
+++ BArrayEj7/src/barrayej7/Empleado.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package barrayej7;
+
+import objects.Fecha;
+
+/**
+ *
+ * @author dam
+ */
+public class Empleado {
+    String nombre;
+    int categoria;
+    Fecha fechaAlta;
+    int numHijos;
+    int horasExtraRealizadas;// menos de 80
+}
